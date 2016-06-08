07207bec3dcfa3eac17f14a44151da18754c5a63
Carlos Frias <carlos.a.frias@gmail.com>
2016-06-08 18:38:42 -0300

    Add initial position

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function() {
+function Enemy() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    var image = 'images/enemy-bug.png',
+        position = randomPos();
+
+    GameObject.call(this, image, position.x, position.y);
 };
+
+Enemy.prototype = Object.create(GameObject.prototype);
+Enemy.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -24,11 +30,40 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+function Player() {
+    var image = 'images/char-boy.png',
+        position = {
+            x: ctx.canvas.clientWidth / 2,
+            y: ctx.canvas.height - 200
+        };
+    GameObject.call(this, image, position.x, position.y);
+}
+
+Player.prototype = Object.create(GameObject.prototype);
+Player.constructor = Player;
+
+Player.prototype.update = function() {
+
+};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function() {
+
+};
+
+function GameObject(image, posX, posY) {
+    this.sprite = image;
+    this.x = posX;
+    this.y = posY;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy(), new Enemy()];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -43,3 +78,19 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// HELPER FUNCTIONS ________________________________________
+function randomPos() {
+    var position = {};
+
+    position.x = 40;
+    position.y = getRandomIntInclusive(100, 200);
+
+    return position;
+}
+
+// Returns a random integer between min (included) and max (included)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
