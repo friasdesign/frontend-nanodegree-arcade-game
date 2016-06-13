427786ac4fc32a0092506f9af5dda8e78117657e
Carlos Frias <carlos.a.frias@gmail.com>
2016-06-13 15:54:51 -0300

    Add comments and create API

---
+++ API/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Collider",
+        "Enemy",
+        "GameObject",
+        "GameObjectInstanciator",
+        "Grid",
+        "GridInstanciator",
+        "InCanvasObject",
+        "Obstacle",
+        "PhysicalObject",
+        "Player"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
